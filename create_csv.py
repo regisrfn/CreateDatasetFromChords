a177f0ed7b5b9ff8c3afe68a3602f21aaab70993
Regis Rufino <regis.rfnrodrigues@gmail.com>
2020-08-01 12:35:47 -0300

    Refactoring

--- create_csv.py
+++ create_csv.py
@@ -2,8 +2,9 @@ import pandas as pd
 import os.path 
 from dataframe_chord import DataFrame
 
-name_chords= ['C']
-folder_chords = f'{os.path.abspath(os.path.dirname(__file__))}/Dataset/Main/C'
+name_chords= ['C/C'] 
+
+folder_chords = f'{os.path.abspath(os.path.dirname(__file__))}/Dataset/Main'
 df = DataFrame(50,folder=folder_chords,chords=name_chords)
 test_df = df.create_dataframe()
-test_df.to_csv('chords_test_2.csv',index=False)
+test_df.to_csv('chords_dataset.csv',index=False)
